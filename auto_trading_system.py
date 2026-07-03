fd65e03e4be735b114221b0fd9c0d30e7c0e1bce
s.yeou <kaljae710@gmail.com>
2026-07-03 13:34:22 +0900

    #2 - sell_nice_timing - feature

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -15,13 +15,26 @@ class AutoTradingSystem:
         raise NotImplementedError
 
     def sell(self, stock_code: str, price: int, count: int) -> None:
-        raise NotImplementedError
+        pass
 
     def get_price(self, stock_code: str) -> int:
-        raise NotImplementedError
+        pass
 
     def buy_nice_timing(self, stock_code: str, amount: int) -> None:
         raise NotImplementedError
 
     def sell_nice_timing(self, stock_code: str, count: int) -> None:
-        raise NotImplementedError
+        prices = []
+        for _ in range(3):
+            prices.append(self._driver.get_price(stock_code))
+            time.sleep(0.2)
+
+        check_sell = True
+        for i in range(2):
+            if prices[i] <= prices[i+1]:
+                check_sell = False
+
+        if check_sell:
+            self._driver.sell(stock_code, prices[-1], count)
+
+
